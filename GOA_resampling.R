71363a04f3d1acb6326a344f263ba0c5e11bdfeb
Rachel Carlson <Rachel.R.Carl@gmail.com>
2022-02-07 01:41:26 -0800

    Generating a larval point cloud from GAO raster imagery (# larvae proportional to coral cover).

--- GOA_resampling.R
+++ GOA_resampling.R
@@ -19,7 +19,26 @@ gao_200m <- aggregate(gao, fact = 100)
 writeRaster(gao_40m, "/Users/rachelcarlson/Documents/Research/RS_data/West_Hawaii_CC/West_Hawaii_Island/LiveCoral/40m_ASU_GAO_Hawaii_SW_Live_v2.tif")
 
 ## Convert raster pixels to point cloud
-points_2m <- rasterToPoints(gao)
+points_40m <- rasterToPoints(gao_40m) # Function to convert raster to an array of xy coordinates (in m) representing the center of each pixel and pixel value.
+sf_40m <- points_40m %>% as.data.frame %>% sf::st_as_sf(coords = c(1,2)) # Function to convert array to sf object (point shapefile)
+colnames(sf_40m) <- c("live_coral", "geometry")
 
+## Create attribute for number of coral larvae each point represents
 
+## The dataframe above has one attribute per point: % live coral cover, ranging from 0 - 64.45%.
+## Larvae at each point is proportional to coral cover. Since there are no fractional larvae,
+## I will first translate coral cover (i.e., larval amount) to the nearest integer.
+## Then, for computational efficiency, I will use each larvae to represent ten larvae, so will find the
+## nearest integer to 1/10 coral cover.
+
+## Translate coral cover to nearest integer.
+sf_40m$larvae <- round(sf_40m$live_coral)
+sum(sf_40m$larvae) # There are 174435 larvae in this version
+
+## Same, but if all larvae represent 10 larvae (faster computation)
+sf_40m$larv_red <- round(sf_40m$larvae/10)
+sum(sf_40m$larv_red) # There are 17051 larvae in this version
+
+## Export to shapefile
+st_write(sf_40m, "/Users/rachelcarlson/Documents/Research/Larvae/Data/Primary/gao_pointcloud.shp")
 
