fd2a4a5cf5e52c24f93a6896855abfc4049ffb2a
Rachel Carlson <Rachel.R.Carl@gmail.com>
2022-03-11 20:00:15 -0800

    Added processing to snap GAO points to nearest non-NA HYCOM data.

--- GOA_resampling.R
+++ GOA_resampling.R
@@ -60,4 +60,42 @@ cloud_40 <- cloud_40 %>% mutate(lat = unlist(map(cloud_40$geometry,2)),
 df <- as.data.frame(cloud_40)[, c(1, 3:6)]
 write.csv(df, "/Users/rachelcarlson/Documents/Research/Larvae/Data/Primary/gao_pointcloud.csv", row.names = FALSE) # row.names = FALSE removes unlabeled index column in CSV
 
+#################### Snap point cloud to non-NA current data
+
+## Read in point cloud
+df <- read.csv("/Users/rachelcarlson/Documents/Research/Larvae/Data/Primary/gao_pointcloud.csv")
+df$newlon <- df$lon + 360 # We'll use -180/180 version here, but this gives us the option of using 0/360
+
+## Convert to point sf object
+sf <- df %>% st_as_sf(coords = c("lon", "lat")) %>%
+  sf::st_set_crs(4326)
+
+## Read in one layer of current data and convert to one big polygon (HYCOM ocean boundary)
+foo <- raster("/Users/rachelcarlson/Documents/Research/Larvae/Data/Primary/oceanographic/HYCOM/Kona_uvwts_300.cdf")
+pp <- rasterToPolygons(foo, dissolve = TRUE)
+pfoo <- pp %>% st_as_sf()
+pfoo2 <- st_union(pfoo)
+plot(pfoo2)
+
+## Intersect points and HYCOM boundary polygon
+sf2 <- st_intersects(sf, pfoo2) # This gives you a list of 15600 points where row is 1 if intersects, (empty) if doesn't intersect
+b <- sapply(sf2,function(x){length(x)==0}) # This gives a logical vector of length 15600 with TRUE/FALSE intersects
+# c <- !b # Use ! because we want the inverse of this vector to find those points that DO NOT intersect
+sf3 <- sf[b,] # Subset our points by only those that are outside the HYCOM ocean boundary
+
+## Snap those points to the nearest HYCOM boundary line
+# This didn't work -- use ArcGIS `Near` tool following directions here https://support.esri.com/en/technical-article/000021426 to find XNear/YNear (new x and y for points outside of HYCOM)
+snapped <- st_read("/Users/rachelcarlson/Documents/Research/Larvae/Data/Primary/gao_pointcloud/gao_pointcloud_180_snapped.shp") %>% 
+  as_data_frame() %>% 
+  select(-c(geometry,NEAR_FID)) %>% # Default geometry is original lat/lon, need to reset
+  st_as_sf(coords = c("NEAR_X", "NEAR_Y")) %>% # Convert to CSV and back to sf with new, nearest HYCOM boundary lat/lon
+  sf::st_set_crs(4326)
+# This only represents those points outside the boundary originally
+
+## Join these points to larger dataset
+sf4 <- sf[!b,]
+GAO_cloud <- bind_rows(sf4,snapped) %>% select(-c(lat,lon))
+sum(!is.na(GAO_cloud$NEAR_DIST)) # Check to make sure this is the # of snapped points (e.g., 7110)
+st_write(GAO_cloud, "/Users/rachelcarlson/Documents/Research/Larvae/Data/Primary/gao_pointcloud/gao_plus_snapped.shp")
+
 
